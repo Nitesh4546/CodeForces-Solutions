d61f8fbc365ac6de89f3745d2785e392f158e5f2
Nitesh@4546 <107245906+Nitesh4546@users.noreply.github.com>
2026-08-27 20:12:10 +0530

    solution for Chunga_Changa
    Chunga_Changa problem solution

---
+++ A_Chunga_Changa.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long sh, ma, co;
+    cin >> sh >> ma >> co;
+
+    long long sh_co = sh % co;
+    long long ma_co = ma % co;
+    long long coconut = (sh + ma) / co;
+    long long min_ = 0;
+
+    if(sh_co + ma_co >= co) {
+        min_ = min(co - sh_co, co - ma_co);
+    }
+    cout << coconut << " " << min_ << "\n";
+    
+    return 0;
+}
