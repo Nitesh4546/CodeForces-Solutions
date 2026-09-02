4a85bca27d58ba8367e0ad418b5cc92df287114f
Nitesh@4546 <107245906+Nitesh4546@users.noreply.github.com>
2026-09-02 22:13:28 +0530

    Extra Terrestrial Intelligence solution
    Solution for Extra Terrestrial Intelligence  in cpp

---
+++ A_Extra_terrestrial_Intelligence.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    if (!(cin >> n)) return 0;
+    string s;
+    cin >> s;
+    
+    vector<int> pos;
+    for (int i = 0; i < n; ++i) {
+        if (s[i] == '1') {
+            pos.push_back(i);
+        }
+    }
+    
+    if (pos.size() < 3) {
+        cout << "NO\n";
+        return 0;
+    }
+    
+    int diff = pos[1] - pos[0];
+    bool pref = true;
+    for (size_t i = 1; i < pos.size() - 1; ++i) {
+        if (pos[i + 1] - pos[i] != diff) {
+            pref = false;
+            break;
+        }
+    }
+    
+    if (pref) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+    
+    return 0;
+}
